d8cd43156b647107d44822a55b6058be54e9c5d8
林创湖 <linchuanghu@addcn.com>
2023-07-28 15:02:31 +0800

    移除V1版本的文件

--- lib/network.dart
+++
@@ -1,7 +0,0 @@
-export 'network/http_request.dart';
-export 'interceptor/interceptor.dart';
-export 'log/options_extra.dart';
-export 'network/http_request_setting.dart';
-export 'safe/safe_convert.dart';
-
-export 'package:dio/dio.dart';
--- test/network_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:network/network.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
