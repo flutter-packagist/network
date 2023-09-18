49315b31eb489c1606a73434bbebd791f546af17
林创湖 <linchuanghu@addcn.com>
2023-09-18 14:42:26 +0800

    feat: 添加单元测试用例

--- lib/resolve/safe_convert.dart
+++ lib/resolve/safe_convert.dart
@@ -1,10 +1,13 @@
-
 int toInt(value, {int defaultValue = 0}) {
   if (value == null) return defaultValue;
   if (value is int) return value;
   if (value is double) return value.toInt();
   if (value is bool) return value ? 1 : 0;
-  if (value is String) return int.tryParse(value) ?? double.tryParse(value)?.toInt() ?? defaultValue;
+  if (value is String) {
+    return int.tryParse(value) ??
+        double.tryParse(value)?.toInt() ??
+        defaultValue;
+  }
   return defaultValue;
 }
 
@@ -55,23 +58,29 @@ int asInt(Map<String, dynamic>? json, String key, {int defaultValue = 0}) {
   return toInt(json[key]);
 }
 
-double asDouble(Map<String, dynamic>? json, String key, {double defaultValue = 0.0}) {
+double asDouble(Map<String, dynamic>? json, String key,
+    {double defaultValue = 0.0}) {
   if (json == null || !json.containsKey(key)) return defaultValue;
   return toDouble(json[key]);
 }
 
-bool asBool(Map<String, dynamic>? json, String key, {bool defaultValue = false}) {
+bool asBool(Map<String, dynamic>? json, String key,
+    {bool defaultValue = false}) {
   if (json == null || !json.containsKey(key)) return defaultValue;
   return toBool(json[key]);
 }
 
-String asString(Map<String, dynamic>? json, String key, {String defaultValue = ""}) {
+String asString(Map<String, dynamic>? json, String key,
+    {String defaultValue = ""}) {
   if (json == null || !json.containsKey(key)) return defaultValue;
   return toString(json[key]);
 }
 
-Map<String, dynamic> asMap(Map<String, dynamic>? json, String key, {Map<String, dynamic>? defaultValue}) {
-  if (json == null || !json.containsKey(key)) return defaultValue ?? <String, dynamic>{};
+Map<String, dynamic> asMap(Map<String, dynamic>? json, String key,
+    {Map<String, dynamic>? defaultValue}) {
+  if (json == null || !json.containsKey(key)) {
+    return defaultValue ?? <String, dynamic>{};
+  }
   return toMap(json[key]);
 }
 
@@ -80,10 +89,47 @@ List asList(Map<String, dynamic>? json, String key, {List? defaultValue}) {
   return toList(json[key]);
 }
 
-List<int> asListInt(Map<String, dynamic>? json, String key, {List? defaultValue}) {
-  return asList(json, key, defaultValue: defaultValue).map((e) => toInt(e)).toList();
+List<int> asListInt(Map<String, dynamic>? json, String key,
+    {List? defaultValue}) {
+  return asList(json, key, defaultValue: defaultValue)
+      .map((e) => toInt(e))
+      .toList();
 }
 
-List<String> asListString(Map<String, dynamic>? json, String key, {List? defaultValue}) {
-  return asList(json, key, defaultValue: defaultValue).map((e) => toString(e)).toList();
+List<String> asListString(Map<String, dynamic>? json, String key,
+    {List? defaultValue}) {
+  return asList(json, key, defaultValue: defaultValue)
+      .map((e) => toString(e))
+      .toList();
+}
+
+T asT<T>(Map<String, dynamic>? json, String key, {T? defaultValue}) {
+  if (json == null || !json.containsKey(key)) {
+    if (defaultValue != null) return defaultValue;
+    if (0 is T) return 0 as T;
+    if (0.0 is T) return 0.0 as T;
+    if ('' is T) return '' as T;
+    if (false is T) return false as T;
+    if ([] is T) return [] as T;
+    if (<String, dynamic>{} is T) return <String, dynamic>{} as T;
+    return '' as T;
+  }
+  dynamic value = json[key];
+  if (value is T) return value;
+
+  if (0 is T) {
+    return toInt(value, defaultValue: (defaultValue ?? 0) as int) as T;
+  } else if (0.0 is T) {
+    return toDouble(value, defaultValue: (defaultValue ?? 0.0) as double) as T;
+  } else if ('' is T) {
+    return toString(value, defaultValue: (defaultValue ?? '') as String) as T;
+  } else if (false is T) {
+    return toBool(value, defaultValue: (defaultValue ?? false) as bool) as T;
+  } else if ([] is T) {
+    return toList(value, defaultValue: (defaultValue ?? []) as List) as T;
+  } else if (<String, dynamic>{} is T) {
+    Object defaultV = defaultValue ?? <String, dynamic>{};
+    return toMap(value, defaultValue: defaultV as Map<String, dynamic>) as T;
+  }
+  return '' as T;
 }
--- test/test.dart
+++ test/test.dart
@@ -1,40 +1,69 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:network/resolve/safe_convert.dart';
+import "package:flutter_test/flutter_test.dart";
+import "package:network/resolve/safe_convert.dart";
 
 void main() {
   test("convert dynamic to safe type", () {
     Object? a;
-    print(a.runtimeType); // print: Null
-    print(toInt(a).runtimeType); // print: int
-    print(toInt(a)); // print: 0
+    assert(a.runtimeType.toString() == "Null"); // print: Null
+    assert(toInt(a).runtimeType.toString() == "int"); // print: int
+    assert(toInt(a) == 0); // print: 0
   });
 
   test("convert from json key to safe type", () {
     final json = {
-      'int': 1,
-      'double': 1.0,
-      'bool': true,
-      'string': 'string',
-      'map': {'key': 'value'},
-      'list': ["1", "2", "3"],
+      "int": 1,
+      "double": 1.0,
+      "bool": true,
+      "string": "string",
+      "map": {"key": "value"},
+      "list": ["1", "2", "3"],
     };
 
-    print(asInt(json, 'int')); // print: 1
-    print(asDouble(json, 'double')); // print: 1.0
-    print(asBool(json, 'bool')); // print: true
-    print(asString(json, 'string')); // print: string
-    print(asMap(json, 'map')); // print: {key: value}
-    print(asList(json, 'list').map((e) => toString(e)).toList()); // print [1, 2, 3]
+    assert(asInt(json, "int") == 1); // print: 1
+    assert(asDouble(json, "double") == 1.0); // print: 1.0
+    assert(asBool(json, "bool") == true); // print: true
+    assert(asString(json, "string") == "string"); // print: string
+    assert(asMap(json, "map").containsKey("key")); // print: {key: value}
+    assert(asList(json, "list").map((e) => toString(e)).toList().join(",") ==
+        "1,2,3"); // print [1, 2, 3]
+
+    print(asMap(json, "int")); // print: {}
+    print(asMap(json, "double")); // print: {}
+    print(asMap(json, "bool")); // print: {}
+    print(asMap(json, "string")); // print: {}
+    print(asMap(json, "map")); // print: {}
+    print(asMap(json, "list")); // print: {}
 
     print("\n");
-    print(asMap(json, 'int')); // print: {}
-    print(asMap(json, 'double')); // print: {}
-    print(asMap(json, 'bool')); // print: {}
-    print(asMap(json, 'string')); // print: {}
-    print(asMap(json, 'map')); // print: {}
-    print(asMap(json, 'list')); // print: {}
+    print(asList(json, "map").map((e) => toString(e)).toList()); // print []
+  });
+
+  test("convert from json key to safe generic type", () {
+    final json = {
+      "int": 1,
+      "double": 1.0,
+      "bool": true,
+      "string": "string",
+      "map": {"key": "value"},
+      "list": ["1", "2", "3"],
+    };
+
+    assert(asT<int>(json, "int") == 1); // print: 1
+    assert(asT<double>(json, "double") == 1.0); // print: 1.0
+    assert(asT<bool>(json, "bool") == true); // print: true
+    assert(asT<String>(json, "string") == "string"); // print: string
+    assert(asT<Map>(json, "map").containsKey("key")); // print: {key: value}
+    assert(asT<List>(json, "list").map((e) => toString(e)).toList().join(",") ==
+        "1,2,3"); // print [1, 2, 3]
+
+    print(asT<Map<String, dynamic>>(json, "int")); // print: {}
+    print(asT<Map<String, dynamic>>(json, "double")); // print: {}
+    print(asT<Map<String, dynamic>>(json, "bool")); // print: {}
+    print(asT<Map<String, dynamic>>(json, "string")); // print: {}
+    print(asT<Map<String, dynamic>>(json, "map")); // print: {}
+    print(asT<Map<String, dynamic>>(json, "list")); // print: {}
 
     print("\n");
-    print(asList(json, 'map').map((e) => toString(e)).toList()); // print []
+    print(asT<List>(json, "map").map((e) => toString(e)).toList()); // print []
   });
 }
